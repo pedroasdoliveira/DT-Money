6618d295df352c104c626360235658345f5f1193
Pedro Oliveira <Pedroaolive@gmail.com>
2024-04-07 18:55:50 -0300

    Feat: modifications and feats add in 07/04

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,35 @@
-import { SummaryContainer } from "./styles";
+import { SummaryContainer, SummaryCard } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 
 const Summary = () => {
   return (
     <SummaryContainer>
-      <div>
+      <SummaryCard>
         <header>
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
         <strong>R$ 1.259,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
         <strong>R$ 16.141,00</strong>
-      </div>
+      </SummaryCard>
     </SummaryContainer>
   );
 };
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
-import Header from "../../components/Header";
-import Summary from "../../components/Summary";
+import Header from '../../components/Header';
+import Summary from '../../components/Summary';
+import { TransactionsContainer, TransactionsTable, PriceHighLight } from "./styles";
 
 const Transactions = () => {
   return (
@@ -7,6 +8,33 @@ const Transactions = () => {
       <Header />
 
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={"50%"}>Desenvolvimento de site</td>
+              <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width={"50%"}>Hamburguer</td>
+              <PriceHighLight variant='outcome'>- R$ 59,00</PriceHighLight>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width={"50%"}>Aluguel do apartamento</td>
+              <PriceHighLight variant='outcome'>- R$ 1.200,00</PriceHighLight>
+              <td>Casa</td>
+              <td>27/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </section>
   );
 };
